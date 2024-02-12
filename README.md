9e5884b4c1e9ee3e927ce7898d57bd614041a80d
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-02-12 15:54:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Microsoft_UI_Clone
+ In this repo i will Pixel perfect Copy Microsoft landing page Ul Clone with full responsive on mutiple device such as 📱, tablet, laptop, 🖥
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-500 font-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea esse
+        a, fugiat numquam totam commodi sapiente distinctio nulla
+        necessitatibus!
+      </h1>
+    </>
+  );
+};
+
+export default App;
